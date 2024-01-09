e996f2d96b9a5cc55937bf3364440f273a351ef3
ErfanMojibi <mojibierfan@gmail.com>
2024-01-09 20:53:30 +0330

    fix typo

--- Agent/Commands/RemoveDirectory.cs
+++ Agent/Commands/RemoveDirectory.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Agent.Models;
 
 namespace Agent.Commands
 {
-    internal class RemoveDirectory
+    public class RemoveDirectory : AgentCommand
     {
+        public override string Name => "rmdir";
+        public override string Execute(AgentTask task)
+        {
+            string path;
+            if (task.Arguments is null || task.Arguments.Length == 0)
+            {
+                return "No path provided";
+            }
+            path = task.Arguments[0];
+            // var recurse = bool.Parse(task.Arguments[1]);
+            //  Directory.Delete(path, recurse);
+            Directory.Delete(path, true); // force recursive delete
+
+            if (!Directory.Exists(path))
+            {
+                return $"{path} deleted";
+            }
+
+            return $"Failed to delete {path}";
+        }
     }
 }

